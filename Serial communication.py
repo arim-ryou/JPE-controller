cf6069096b9313bcccdd49617875d25dc650b1ed
arim-ryou <79498334+arim-ryou@users.noreply.github.com>
2024-01-01 15:52:11 +0900

    Update Serial communication.py

--- Serial communication.py
+++ Serial communication.py
@@ -33,8 +33,11 @@ motion_con_frame = tk.Frame(window)
 notebook.add(motion_con_frame, text="Motion Control")
 
 ## 프리셋 값
+stage = "CLA2602"
 optCom = tk.StringVar()
 optCom.set('1')
+optBr = tk.StringVar()
+optBr.set('115200')
 optFreq = tk.StringVar()
 optFreq.set('600')
 optTemp = tk.StringVar()
@@ -43,6 +46,16 @@ optRss = tk.StringVar()
 optRss.set('100')
 optDf = tk.StringVar()
 optDf.set('1.0')
+
+optDir_1= tk.IntVar()
+optSteps_1 = tk.StringVar()
+optSteps_1.set('0')
+optDir_2= tk.IntVar()
+optSteps_2 = tk.StringVar()
+optSteps_2.set('0')
+optDir_3= tk.IntVar()
+optSteps_3 = tk.StringVar()
+optSteps_3.set('0')
 
 # 기능 모음
 ## Serial communication Frame 기능
@@ -86,6 +99,34 @@ def Command_state(address): # 상태 확인하기
     else: 
         respond_text_2.insert('end', response_result, "Error")
 
+def Command_move(address):
+    if address == 1:
+        optDir =  optDir_1.get()
+        optSteps = optSteps_1.get()
+    elif address == 2: 
+        optDir =  optDir_2.get()
+        optSteps = optSteps_2.get()       
+    else:
+        optDir =  optDir_3.get()
+        optSteps = optSteps_3.get()
+
+    command = "MOV %i %i %s %s %s %s %s %s " % (address, optDir, optFreq.get(), optRss.get(), optSteps, optTemp.get(), stage, optDf.get())
+    command_result, response_result, error_flag = commanding(command)
+    respond_text_2.insert('end', command_result, "Command")
+    if error_flag == 0:
+        respond_text_2.insert('end', response_result, "Response")
+    else: 
+        respond_text_2.insert('end', response_result, "Error")
+
+def command_stop(address):
+    command = "STP "+str(address)
+    command_result, response_result, error_flag = commanding(command)
+    respond_text_2.insert('end', command_result, "Command")
+    if error_flag == 0:
+        respond_text_2.insert('end', response_result, "Response")
+    else: 
+        respond_text_2.insert('end', response_result, "Error")   
+
 
 ###################################-- Serial Communication Frame 구성 --##########################################
 
@@ -97,8 +138,8 @@ set_up_frame.pack(side = 'left')
 label_port = tk.Label(master = set_up_frame, text="COM port", font = Bold_font, bg= "gray80")
 input_port = tk.Spinbox(master = set_up_frame, from_= 0, to = 100, textvariable= optCom, font=Light_font)
 label_baudrate = tk.Label(master = set_up_frame, text = "Baudrate", font = Bold_font, bg= "gray80")
-input_baudrate = tk.Entry(master = set_up_frame, font= Light_font)
-input_baudrate.insert(0, "115200")   
+input_baudrate = tk.Spinbox(master = set_up_frame, from_= 9600, to = 1000000, textvariable= optBr,font= Light_font)
+
 
 butTxtRespClear = tk.Button( master = set_up_frame, text="Clear command history", command = txtResp_clear_click, font = Bold_font)
 
@@ -138,14 +179,14 @@ port_frame.place(x=0, y=0)
 label_port_2 = tk.Label(master = port_frame, text="COM port", font = Bold_font, bg= "gray80")
 input_port_2 = tk.Spinbox(master = port_frame, from_= 0, to = 100, textvariable=optCom, font=Light_font)
 label_baudrate_2 = tk.Label(master = port_frame, text = "Baudrate", font = Bold_font, bg= "gray80")
-input_baudrate_2 = tk.Entry(master = port_frame, font= Light_font)
-input_baudrate_2.insert(0, "115200") 
+input_baudrate_2 = tk.Spinbox(master = port_frame, from_= 9600, to = 1000000, textvariable=optBr, font= Light_font)
+
 butTxtRespClear_2 = tk.Button( master = port_frame, text="Clear command", command = txtResp_clear_click_2, font = Bold_font)
 
-label_port_2.place(x=5, y= 5, width=80, height=30)
-input_port_2.place(x = 90, y = 5, width=60, height=30)
-label_baudrate_2.place(x=5, y= 40, width=80, height=30)
-input_baudrate_2.place(x=90, y= 40, width=60, height=30)
+label_port_2.place(x=5, y= 5, width= 150, height=30)
+input_port_2.place(x = 5, y = 35, width= 150, height=30)
+label_baudrate_2.place(x=5, y= 80, width= 150, height=30)
+input_baudrate_2.place(x=5, y= 110, width= 150, height=30)
 butTxtRespClear_2.place(x= 5, y = 165, width=150, height= 30)
 
 # 환경설정 프레임 구성
@@ -185,40 +226,40 @@ butTxtRespClear = tk.Button( master = command_fram_2, text="Clear command histor
 respond_text_2.place(x= 5, y = 5, width = 870, height= 360)
 scroll_2.place(x = 880, y = 5, height= 360)
 
-
 ##  Address 
 class motion_frame():
-    def __init__(self):
+    def __init__(self, optDir, optSteps):
         self.width = 246
         self.height = 160
-        self.stage = "CLA2602"
-        self.optSteps = tk.StringVar()
-        self.optSteps.set('0')
-    
+        self.optDir= optDir
+        self.optSteps = optSteps
 
     def making_address_frame(self, address):
         Address_frame = tk.Frame(motion_con_frame, width = self.width, height=self.height, relief="groove", bg= "gray90", bd=1)
         Address_frame.place(x = 160 + self.width*(address-1) , y = 40)
 
-        label_addr = tk.Label(master=Address_frame, text ="Adress number " + str(1), font= Bold_font, bg= "gray90")
-        check_dir = tk.Checkbutton(master=Address_frame, text= "Direction", font = Bold_font, bg= "gray90")
+        label_addr = tk.Label(master=Address_frame, text ="Address number " + str(1), font= Bold_font, bg= "gray90")
+        check_dir = tk.Checkbutton(master=Address_frame, text= "Positive Direction", variable= self.optDir,font = Bold_font, bg= "gray90")
         label_steps = tk.Label(master=Address_frame, text ="Steps", font= Bold_font, bg= "gray90")
         input_steps = tk.Spinbox(master = Address_frame, from_ = 0, to = 50000, textvariable=self.optSteps, font = Light_font)
-        button_GFS = tk.Button(master = Address_frame, text= "State",overrelief="solid", repeatdelay=1000, repeatinterval=1000, font = Bold_font, command=partial(Command_state, address))
-        button_start = tk.Button(master = Address_frame, text= "Start",overrelief="solid", repeatdelay=1000, repeatinterval=1000, font = Bold_font)
-        button_stop = tk.Button(master = Address_frame, text= "Stop",overrelief="solid", repeatdelay=1000, repeatinterval=1000, font = Bold_font)
+        button_GFS = tk.Button(master = Address_frame, text= "State",overrelief="solid", repeatdelay=1000, repeatinterval=1000, font = Bold_font, \
+                               command=partial(Command_state, address))
+        button_mov = tk.Button(master = Address_frame, text= "Move",overrelief="solid", repeatdelay=1000, repeatinterval=1000, font = Bold_font, \
+                               command=partial(Command_move, address))
+        button_stop = tk.Button(master = Address_frame, text= "Stop",overrelief="solid", repeatdelay=1000, repeatinterval=1000, font = Bold_font, \
+                                command = partial(command_stop, address))
 
         label_addr.place(x = 10 , y = 10, height= 30)
         check_dir.place(x = 10, y = 50 )
         label_steps.place(x = 10, y = 90, height= 30)
         input_steps.place(x = 100, y = 90, width = 120, height= 30)
         button_GFS.place(x = 0, y = 130, width = 82, height = 30)
-        button_start.place(x = 82, y = 130, width= 82, height= 30)
+        button_mov.place(x = 82, y = 130, width= 82, height= 30)
         button_stop.place(x = 164, y = 130, width= 82, height= 30)
 
-address_frame_1 = motion_frame()
-address_frame_2 = motion_frame()
-address_frame_3 = motion_frame()
+address_frame_1 = motion_frame(optDir_1,  optSteps_1)
+address_frame_2 = motion_frame(optDir_2, optSteps_2)
+address_frame_3 = motion_frame(optDir_3, optSteps_3)
 
 address_frame_1.making_address_frame(1)
 address_frame_2.making_address_frame(2)
